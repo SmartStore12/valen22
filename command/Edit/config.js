db51b7fcf039b5096c2131ca546490650f3e7c42
SmartStore12 <117746944+SmartStore12@users.noreply.github.com>
2022-11-27 18:31:04 +0700

    Update config.js

--- command/Edit/config.js
+++ command/Edit/config.js
@@ -1,23 +1,23 @@
 const { modul } = require('../Edit/module');
 const { chalk, fs, proces } = modul;
 
-global.ownerNumber = ["6282399255216@s.whatsapp.net"]
-global.namabotnya = 'Cyber Is Real'
-global.namaownernya = 'Cyber Executed'
+global.ownerNumber = ["6281290802585@s.whatsapp.net"]
+global.namabotnya = 'SmartStore Anonymous'
+global.namaownernya = 'SmartStore Jomblo'
 global.packname = '\n\n\n\n\n\n\n\n\n\n© Cyber||+62🇲🇨\nI`m From Indonesia\n\n\n\n\n\n\n\n\n\n'
 global.author = '\n\n\n\n\n\n\n\n\n\nWa : 0823-9925-5216\nYT : Cyber S-C-D-B\n\n\n\n\n\n\n\n\n\n'
 global.sessionName = 'CyberExe'
 global.lolkey = 'Atakbotz'
 global.dapkey = 'Kirbotz123'
 global.kirkey = 'KirBotz'
-global.email = 'id3026799@gmail.com'
+global.email = 'KagaTau22@gmail.com'
 global.group = 'https://bit.ly/38oNU0D'
 global.youtube = 'https://bit.ly/39Ivus6'
 global.website = 'https://bit.ly/3yk3gOo'
 global.github = 'https://bit.ly/3FqVBj8'
-global.noown = 'https://wa.me/6282399255216'
-global.region = 'I`m From Indonesia'
-global.prefa = ['','!','.','#','-','�?']
+global.noown = 'https://wa.me/6281290802585'
+global.region = 'I`m From Cindo'
+global.prefa = ['','!','.','#','-','�?']
 global.api = 
 {
     success: '```Success✅```',
